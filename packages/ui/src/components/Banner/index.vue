81ed14b153f627e09c049481e43ac2d83ec7d0b2
Hieu Tran <hieutran.fu@gmail.com>
2019-10-03 10:51:48 +0700

    feat(ui): add banner component

---
+++ packages/ui/src/components/Banner/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="av-banner" :class="classes">
+    <div class="av-banner-container" role="alert">
+      <div class="av-banner-content">
+        <div class="av-banner-text">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js" />
+<style src="./index.scss" lang="scss">
